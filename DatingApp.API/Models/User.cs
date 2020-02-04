3f3c659603f08abaa5a7ff32eb41fdf7c0256fc7
HagayRatzabi <hr4020@gmail.com>
2020-02-04 23:38:55 +0200

    till 26

---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    pקublic class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
